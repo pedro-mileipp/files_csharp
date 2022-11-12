3287c4b4f5c74f1347893c26751900399dc66f26
pedro-mileipp <poncianopedro70@gmail.com>
2022-11-12 11:54:09 -0300

    como usar StreamWritter

--- Program.cs
+++ Program.cs
@@ -3,36 +3,24 @@ using System.IO;
 
 class Program{
     static void Main(){
-        // using block
         /*
-            Sintaxe simplificada que garante que os objetos IDisposable serão fechados.
-            Objetos IDisposable não são gerenciados pelo CLR, portanto precisam ser manualmente fechados. 
-            Exemplos: Font, FileStream, StreamReader, StreamWritter
+        É uma stream capaz de escrever caracteres a partir de uma stream binária (ex: FileStream)
+        Suporte a dados no formato de texto.
         */
-        try{
-            string path = @"C:\Users\Pedro\Documents\aquivos_c#\file1.txt";
-            using (FileStream fs = new FileStream(path, FileMode.Open)){ // ao acabar de usar o using fecha automaticamente
-                using (StreamReader sr = new StreamReader(fs)){
-                    while (!sr.EndOfStream){
-                        string line = sr.ReadLine();
-                        Console.WriteLine(line);
-                    }
-                }
-            }
-        } catch(IOException e){
-            Console.WriteLine("An error occured");
-            Console.WriteLine(e.Message);
-        }
 
-        // segunda forma
-        string path2 = @"C:\Users\Pedro\Documents\aquivos_c#\file2.txt";
+        // ler o conteúdo de file1 e transformar em uppercase para file2
+        string sourcePath = @"C:\Users\Pedro\Documents\aquivos_c#\file1.txt";
+        string targetPath = @"C:\Users\Pedro\Documents\aquivos_c#\file2.txt";
+
         try{
-            using (StreamReader sr2 = File.OpenText(path2)){
-                while(!sr2.EndOfStream){
-                    string line2 = sr2.ReadLine();
-                    Console.WriteLine(line2);
+            string[] lines = File.ReadAllLines(sourcePath);
+            
+            using (StreamWriter sw = File.AppendText(targetPath)){
+                foreach(string line in lines){
+                    sw.WriteLine(line.ToUpper());
                 }
             }
+
         } catch(IOException e){
             Console.WriteLine("An error occured");
             Console.WriteLine(e.Message);
