fa4ff8e6c83d7f62b9c5b9a18911d078c9b34a53
pedro-mileipp <poncianopedro70@gmail.com>
2022-11-12 11:31:50 -0300

    bloco using, como usar

--- Program.cs
+++ Program.cs
@@ -3,45 +3,40 @@ using System.IO;
 
 class Program{
     static void Main(){
-        string filePath = @"C:\Users\Pedro\Documents\aquivos_c#\file1.txt";
-        string targetPath = @"C:\Users\Pedro\Documents\aquivos_c#\file2.txt";
-        // string targetPath2 = @"C:\Users\Pedro\Documents\aquivos_c#\file3.txt";
-
+        // using block
+        /*
+            Sintaxe simplificada que garante que os objetos IDisposable serão fechados.
+            Objetos IDisposable não são gerenciados pelo CLR, portanto precisam ser manualmente fechados. 
+            Exemplos: Font, FileStream, StreamReader, StreamWritter
+        */
         try{
-            if (!File.Exists(targetPath)){
-                FileInfo test_fileInfo = new FileInfo(filePath);
-                test_fileInfo.CopyTo(targetPath);
+            string path = @"C:\Users\Pedro\Documents\aquivos_c#\file1.txt";
+            using (FileStream fs = new FileStream(path, FileMode.Open)){ // ao acabar de usar o using fecha automaticamente
+                using (StreamReader sr = new StreamReader(fs)){
+                    while (!sr.EndOfStream){
+                        string line = sr.ReadLine();
+                        Console.WriteLine(line);
+                    }
+                }
             }
-            string[] all_lines = File.ReadAllLines(filePath);
-            foreach (string lines in all_lines){
-                System.Console.WriteLine(lines);
-            }
-
-            // if(!File.Exists(targetPath2)){
-            //     File.Copy(filePath, targetPath2);
-            // }
-
-        } catch (IOException e){
+        } catch(IOException e){
             Console.WriteLine("An error occured");
             Console.WriteLine(e.Message);
         }
 
-        string stream_path = @"C:\Users\Pedro\Documents\aquivos_c#\stream_file1.txt"; 
-        FileStream fs = null;
-        StreamReader sr = null;
+        // segunda forma
+        string path2 = @"C:\Users\Pedro\Documents\aquivos_c#\file2.txt";
         try{
-            fs = new FileStream(stream_path, FileMode.Open);
-            sr = new StreamReader(fs);
-            string line = sr.ReadLine();
-            Console.WriteLine(line);
-            
-        }catch(IOException e){
+            using (StreamReader sr2 = File.OpenText(path2)){
+                while(!sr2.EndOfStream){
+                    string line2 = sr2.ReadLine();
+                    Console.WriteLine(line2);
+                }
+            }
+        } catch(IOException e){
             Console.WriteLine("An error occured");
             Console.WriteLine(e.Message);
-
-        } finally{
-            if (sr != null) sr.Close();
-            if (fs != null) fs.Close();
         }
+        
     }
 }
